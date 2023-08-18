9fd3dba95ff5018a6374bede19313e94ad0ecffe
olga-007 <129122802+olga-007@users.noreply.github.com>
2023-08-17 21:24:20 -0700

    Add restaurant title

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import './styles.css';
 
-console.log('Hi there!');
+function addNewElement(type, parent, cssClass) {
+    const element = document.createElement(type);
+    if (cssClass) {
+        element.classList.add(cssClass);
+    }
+    parent.appendChild(element);
+    return element;
+}
+
+function addNewDiv(parent, cssClass) {
+    return addNewElement('div', parent, cssClass);
+}
+
+const contentDiv = document.getElementById('content');
+
+const titleBox = addNewDiv(contentDiv, 'box');
+const titleDiv = addNewDiv(titleBox, 'title');
+titleDiv.textContent = 'Awesome Restaurant';
