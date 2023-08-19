e732f9eada1e719e3c02d38d719ba1b836e31bee
olga-007 <129122802+olga-007@users.noreply.github.com>
2023-08-19 11:03:34 -0700

    Add tab content

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,6 @@
 import { addNewDiv } from './helper';
 
 export default function render(parent) {
-    const textDiv = addNewDiv(parent);
-    textDiv.textContent = 'Contact content';
+    const textDiv = addNewDiv(parent, 'tab-text');
+    textDiv.textContent = 'Contact us at ThisTotallyNotFake@email.com';
 }
--- src/menu.js
+++ src/menu.js
@@ -1,6 +1,11 @@
-import { addNewDiv } from './helper';
+import { addNewDiv, addNewElement } from './helper';
 
 export default function render(parent) {
-    const textDiv = addNewDiv(parent);
-    textDiv.textContent = 'Menu content';
+    const textDiv = addNewDiv(parent, 'tab-text');
+
+    const p1 = addNewElement('p', textDiv);
+    p1.textContent = 'Indulge in our carefully curated menu, a harmonious blend of classic favorites and inventive creations. From hearty farmhouse soups to artisanal wood-fired pizzas, each dish tells a story of tradition and innovation.';
+
+    const p2 = addNewElement('p', textDiv);
+    p2.textContent = 'Not sure what to choose? Say: "Surprise me!"';
 }
